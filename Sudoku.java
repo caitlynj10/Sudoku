c1e55b372613303eb97cd9e7ccde3fc9614109f1
Your Name <caitlynj@brandeis.edu>
2026-04-15 14:00:25 -0400

    Sudoku logic done

--- Sudoku.java
+++ Sudoku.java
@@ -1,13 +1,38 @@
+import java.util.*;
+
 class SudokuGame{
     
+    
+    
     public int[][] createBoard(){
-        int [][] board = new int[9][9];
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[i].length; j++){
-                board[i][j] = (int)(Math.random() * 10);
+        int[][] board = new int[9][9];
+        checkValidBoard(board);
+    
+        return board;
+    }
+
+    public boolean checkValidBoard(int[][] board){
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == 0) { 
+                    List<Integer> nums = new ArrayList<>();
+                    for (int k = 1; k <= 9; k++) nums.add(k);
+                    Collections.shuffle(nums);
+                    for (int num : nums) {
+                        if (checkRows(num, board, i) && checkColumns(num, board, j) && checkSquare(num, board, i, j)) {
+                            board[i][j] = num;
+                            if (checkValidBoard(board)) { 
+                                return true;
+                            }
+                            board[i][j] = 0; 
+                        }
+                    }
+                    return false; 
+                }
             }
         }
-        return board;
+
+        return true;
     }
 
     public void printBoard(int[][] board){  
@@ -19,20 +44,42 @@ class SudokuGame{
         }
 
     }
-    public boolean checkRows(int[] row) {
+
+    public boolean checkRows(int rowVal, int[][] board, int row) {
+        for(int i = 0; i<9; i++){
+            if(board[row][i] == rowVal){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean checkColumns(int colVal, int[][] board, int col){
+        for(int j = 0; j<9; j++){
+            if(board[j][col] == colVal){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    
+    public boolean checkSquare(int num, int[][] board, int row, int col){
+        int startRow = row - (row % 3);
+        int startCol = col - (col % 3);
+        for(int i = startRow; i < startRow + 3; i++){
+            for(int j = startCol; j < startCol + 3; j++){
+                if(board[i][j] == num){
+                    return false;
+                }
+            }
+                
+        }
         
-        return true;
-    }
-
-    public boolean checkColumns(int[] col){
 
         return true;
     }
-
-    public boolean checkSquare(int[][] board){
-
-        return true;
-    }
+    
     
     public boolean fillBoard(){
         return true;
